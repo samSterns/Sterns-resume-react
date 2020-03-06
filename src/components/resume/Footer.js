e732ddf93ffe11f5ec846c4314bfbb5847dc7289
samSterns <sam.g.sterns@gmail.com>
2020-03-06 09:50:26 -0800

    footer fixed again addaed

--- src/components/resume/Footer.js
+++ src/components/resume/Footer.js
@@ -5,7 +5,7 @@ import { FaFileDownload, FaGithub, FaLinkedin } from 'react-icons/fa';
 const Footer = () => {
 
   const downloadResume = () => {
-    window.location.href = './styles/assets/SamSternsResume.pdf'
+    window.location.href = 'https://samgsterns.com./src/components/styles/assets/SamSternsResume.pdf';
   };
 
   return (
@@ -13,14 +13,14 @@ const Footer = () => {
       <footer className={styles.Footer} >
         <form>
           <button onClick={downloadResume}>
-            <FaFileDownload alt="click to download my resume" className={styles.icon} size={32}/>
+            <FaFileDownload alt="click to download my resume" className={styles.icon} size={36}/>
           </button>
         </form>
         <a href={'https://github.com/samSterns'}>
-          <FaGithub alt="click to Visit my git hub" className={styles.icon} size={32}/>
+          <FaGithub alt="click to Visit my git hub" className={styles.icon} size={36}/>
         </a>
         <a href={'https://www.linkedin.com/in/samsterns/'}>
-          <FaLinkedin alt="click to Visit my Linked In" className={styles.icon} size={32}/>
+          <FaLinkedin alt="click to Visit my Linked In" className={styles.icon} size={36}/>
         </a>
       </footer>
     </>
