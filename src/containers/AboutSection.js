181cca0a574fbe60e90c68064df98f621511d8ad
samSterns <sam.g.sterns@gmail.com>
2020-03-14 12:49:42 -0700

    new values illustrations added

--- src/containers/AboutSection.js
+++ src/containers/AboutSection.js
@@ -3,14 +3,17 @@ import React from 'react';
 
 import styles from './AboutSection.css';
 
+
 export default function AboutSection() {
 
   return (
-
-    
-    
-     
-      <div>XXXXXXX</div>
+    <section className={styles.About}>
+      <figure>
+        <img src="./succulent.png" alt="Trulli" style={{ width: '100%' }} />
+        <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
+      </figure>
+    </section>
+      
 
       
  
