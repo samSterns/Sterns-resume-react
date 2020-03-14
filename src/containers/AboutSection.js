53bd17db0a51f0d207eb874d023671e694c7ab16
samSterns <sam.g.sterns@gmail.com>
2020-03-14 14:20:41 -0700

    value cards added with skeleton copy needs media query

--- src/containers/AboutSection.js
+++ src/containers/AboutSection.js
@@ -1,18 +1,28 @@
 import React from 'react';
-
-
 import styles from './AboutSection.css';
-
+import resImg from './asset/res.png';
+import com from './asset/com.png';
+import growth from './asset/growth.png';
 
 export default function AboutSection() {
 
   return (
     <section className={styles.About}>
-      <figure>
-        <img src="./succulent.png" alt="Trulli" style={{ width: '100%' }} />
-        <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
-      </figure>
+      <div className={styles.Value}>
+        <p> XXXXX</p>
+        <img src={resImg} />
+      </div>
+      <div className={styles.Value}>
+        <p> XXXXX</p>
+        <img src={com} />
+      </div>
+      <div className={styles.Value}>
+        <p> XXXXX</p>
+        <img src={growth} />
+      </div>
     </section>
+
+   
       
 
       
