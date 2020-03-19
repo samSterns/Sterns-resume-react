798a9ac3948854338e6eb2a6563a26bd956b58a6
samSterns <sam.g.sterns@gmail.com>
2020-03-18 19:21:18 -0700

    outside the bounds

--- src/containers/AboutSection.js
+++ src/containers/AboutSection.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import styles from './AboutSection.css';
 import resImg from './asset/res.png';
-import com from './asset/com.png';import growth from './asset/growth.png';
+import com from './asset/com.png';
+import growth from './asset/growth.png';
 
 export default function AboutSection() {
 
   return (
     <section className={styles.container}>
-      <div className={styles.gridRow}>
-        <div className={styles.gridItem} >
-          <img src={resImg} />
-          <p className={styles.AboutP}> I have a deep sense of responsibility to leave the world a better place, which shines through in my personal actions and professional integrity. </p>
-          <p className={styles.AboutP}>20+ years volunteering with youth</p>
-        </div>
-        <div className={styles.gridItem}>
-          <img src={com} />
-          <p className={styles.AboutP}> I listen and hold space for people to feel heard and plan effective communication for community development. </p>
-          <p className={styles.AboutP}>
-            4+ years of experience as a Restorative Justice conflict mediator in the public and private sectors </p>
-        </div>
-        <div className={styles.gridItem}>
-          <img src={growth} />
-          <p className={styles.AboutP}> I believe in continuous improvement by staying grounded in self-awareness while both accepting and giving radical candor. </p>
-          <p className={styles.AboutP}>Devoted 800+ hours to learning to code in less than 6 months</p>
-        </div>
+      <div className={styles.gridItem} >
+        <img src={resImg} />
+        <p className={styles.AboutP}> I have a deep sense of responsibility to leave the world a better place, which shines through in my personal actions and professional integrity. </p>
+        <p className={styles.AboutP}>20+ years volunteering with youth</p>
       </div>
+      <div className={styles.gridItem}>
+        <img src={com} />
+        <p className={styles.AboutP}> I listen and hold space for people to feel heard and plan effective communication for community development. </p>
+        <p className={styles.AboutP}>4+ years of experience as a Restorative Justice conflict mediator in the public and private sectors </p>
+      </div>
+      <div className={styles.gridItem}>
+        <img src={growth} />
+        <p className={styles.AboutP}> I believe in continuous improvement by staying grounded in self-awareness while both accepting and giving radical candor. </p>
+        <p className={styles.AboutP}>Devoted 800+ hours to learning to code in less than 6 months</p>
+      </div>
+
     </section>
    
   );
--- src/containers/ProjectSection.js
+++ src/containers/ProjectSection.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Project } from '../components/projectsSection/ProjectItem';
+import { Project1, Project2, Project3, Project4 } from '../components/projectsSection/ProjectItem';
 import styles from './ProjectSection.css';
 import data from '../resume.json';
 
@@ -10,10 +10,10 @@ export default function ProjectSection() {
     <>
       <h2 className={styles.ProjectListHeader}>Projects</h2>
       <section className={styles.ProjectList} >
-        <Project className={styles.ProjectListItem} {...projectPetPals} />
-        <Project className={styles.ProjectListItem} {...projectCovrLettr} />
-        <Project className={styles.ProjectListItem} {...projectLyricus} />
-        <Project className={styles.ProjectListItem} {...projectFTM} />
+        <Project1 className={styles.ProjectListItem} {...projectPetPals} />
+        <Project2 className={styles.ProjectListItem} {...projectCovrLettr} />
+        <Project3 className={styles.ProjectListItem} {...projectLyricus} />
+        <Project4 className={styles.ProjectListItem} {...projectFTM} />
       </section>
     </>
   );
