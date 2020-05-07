efd25a9a6b939288fdb47f9405c77d42693351c5
samSterns <sam.g.sterns@gmail.com>
2020-05-07 12:46:43 -0700

    router puzzels abound but the semantic bones are done

--- src/components/projectsSection/ProjectDetail.js
+++ src/components/projectsSection/ProjectDetail.js
@@ -1,29 +1,52 @@
 import React from 'react';
-// import { FaGithub } from 'react-icons/fa';
+import { FaGithub, FaHome, FaLink } from 'react-icons/fa';
+import imageP from '../landingPage/styles/assets/petPals.gif';
+import imageCL from '../landingPage/styles/assets/CL.png';
+import imageL from '../landingPage/styles/assets/lyricus.gif';
+import imageFTM from '../landingPage/styles/assets/followMoney.gif';
+import styles from '../projectsSection/projectDetail.css';
 
 export const ProjectDetailPetPals = ({ match }) => {
   return (
-    <div className={styleMedia.ProjectDetailPetPals} >
-      <aside>
-        <img src={imageP} alt="GIF previewing Pet Pals project" />
-      </aside>
-      <article>
-        <title> </title>
-        <subtitle></subtitle>
-        <section> 
+    <div className={styles.projectDetailContainer}>
+      <header>
+        <a>
+          <FaHome alt="click here to go back home" size={36}/>
+        </a>
+      </header>
+      <section>
+        <header>Pet Pals</header>
+        <figure>
+          <img src={imageP} alt="GIF previewing Pet Pals project" />
+        </figure>
+        <a><FaLink alt="click here to Visit this project's deployed site" size={36}/>Live Site</a>
+        <a className={styles.ProjectLink} href={githubURL} target="_blank">
+          <FaGithub alt="click here to Visit this project's git hub" size={36}/>Git Hub </a>
+
+        <article>
           <ul>
-            <li> </li>
+            <li>Cooperative dog walking React.js/Redux CRUD web application
+Detailed user profiles saved in MongoDB/mongoose schemas and AWS S3 storage for the images of the user and their pup </li>
+            <li>Sass styling utilizing specifically defined variable names </li>
+            <li>Agile team of 5 Alchemy Code Lab students in a week-long sprint </li>
+            <li>I contributed the real-time messaging feature with Socket.io, Node.js, and Express</li>
+            <li>I researched and designed the UX architecture</li>
           </ul>
-          <button size="small" color="primary"  target="_blank">
-            <a className={styles.ProjectLink} href={githubURL} target="_blank">
-              <FaGithub alt="click here to Visit this project's git hub" size={36}/> </a>
-          </button>
-        </section>
-      </article>
-    </div> 
+        </article>
+
+
+      </section>
+    </div>
   );
 };
 
+// A mobile-first & accessibility-friendly vanilla JavaScript web app
+// Data from the third party Federal Elections Commission's API
+// Sankey graph displays custom data visualizations that demonstrate the dollar amount of contributions to the primary presidential campaigns
+// Users enter job specific details to generate original cover letters
+// Backend 4 day student group project w/ MongoDB, Express, Node.js
+// NPM natural language processing sentiment analysis scores over a million variations of letters, and outputs the highest-scoring one
+// I led the team from ideation through development to production
 
 {/* <Typography gutterBottom variant="h5" component="h3">
   <h3><a className={styles.ProjectLink}  href={siteURL} target="_blank"> {header}</a></h3>
