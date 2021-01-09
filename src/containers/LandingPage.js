5470cc0651b3b34ace78a43fa0fb7516a678f979
samSterns <sam.g.sterns@gmail.com>
2021-01-08 16:12:55 -0800

    deploy now with this version as placeholder

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
-import Header from '../components/landingPage/Header';
 import Profile from '../components/landingPage/Profile';
 import NavBar from '../components/landingPage/NavBar';
-
-// import TechSkill from '../components/resume/TechSkill';
-// import { ExperienceItem1, ExperienceItem2, ExperienceItem3, ExperienceItem4 }  from '../components/resume/ExperienceItem';
-// import ProjectItem from '../components/resume/ProjectItem';
 import styles from './LandingPage.css';
-import data from '../resume.json';
 
+import Pic from '../components/landingPage/styles/assets/selfPortrait.png';
 export default class LandingPage extends Component {
 
   render(){
-    const { header, profile } = data;
-
-
 
     return (
-      < div className={styles.Hero} >
+      <main className={styles.Card}>
+        <img className={styles.ProfileImg} src={Pic} />
+        <p>Sam Sterns is a Product Designer & Front End Developer who believes technology is a tool to build equity, which is why they focus on creating inclusive, impactful, and maintainable digital products.  New portfolio projects coming soon.</p>
         <NavBar />
-        <Header {...header}/>
-        
-        <Profile {...profile}/>
-      </div>
+      </main>
+       
     );
     
   }
